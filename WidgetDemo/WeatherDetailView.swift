24b75a0d2ddbd6dc182173105907b9f4146059c4
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-28 04:49:09 +0600

    WeatherDetailsView added

---
+++ WidgetDemo/WeatherDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherDetailView.swift
+//  WidgetDemo
+//
+//  Created by JOYNAL ABEDIN on 28/9/22.
+//
+
+import SwiftUI
+
+struct WeatherDetailView: View {
+    var name: String
+    var icon: String
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherDetailView(name: "", icon: "")
+    }
+}
