c7d8368c632d1aa97f47028f061832439a5867d2
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-28 04:54:06 +0600

    ui implemented in weatherDetailsView

--- WidgetDemo/WeatherDetailView.swift
+++ WidgetDemo/WeatherDetailView.swift
@@ -11,12 +11,23 @@ struct WeatherDetailView: View {
     var name: String
     var icon: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: icon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+            Text(name)
+                .padding()
+                .font(.title)
+            Text("If this were a real weather app, a description of \(name) would appear here.")
+                .padding()
+            Spacer()
+        }
     }
 }
 
 struct WeatherDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        WeatherDetailView(name: "", icon: "")
+        WeatherDetailView(name: "Thunder Storems", icon: "cloud.bolt")
     }
 }
